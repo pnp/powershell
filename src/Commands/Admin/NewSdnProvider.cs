32dae3f5369171dcb36f880a6b3084c2552bd781
Erwin van Hunen <erwin.van.hunen@outlook.com>
2020-10-11 23:04:03 +0200

    Added New-PnPSdnProvider cmdlet

---
+++ src/Commands/Admin/NewSdnProvider.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.SharePoint.Client;
+using PnP.PowerShell.Commands.Base;
+using System.Management.Automation;
+
+namespace PnP.PowerShell.Commands.Admin
+{
+    [Cmdlet(VerbsCommon.New, "SdnProvider", SupportsShouldProcess = true)]
+    public class NewSdnProvider : PnPAdminCmdlet
+    {
+        [Parameter(Mandatory = true)]
+        public string Identity;
+
+        [Parameter(Mandatory = true)]
+        public string License;
+
+        protected override void ExecuteCmdlet()
+        {
+            if (ShouldProcess("Adds a new SDN Provider"))
+            {
+                this.Tenant.AddSdnProvider(Identity, License);
+                ClientContext.ExecuteQueryRetry();
+            }
+        }
+    }
+}
