46553f2d3f759cf4166dc636a937b580b9ff3790
Erwin van Hunen <erwin.van.hunen@outlook.com>
2020-10-17 16:23:26 +0200

    updated build script

--- pages/Build-Site.ps1
+++ pages/Build-Site.ps1
@@ -1,17 +1,38 @@
 # copy documentation to output folder
 # make cmdlets folder
 New-Item -Path "./dev/pages/cmdlets" -ItemType Directory
-Copy-Item -Path "./dev/documentation/*.md" -Destination "./dev/pages/cmdlets" -Force
+New-Item -Path "./dev/pages/cmdlets/released" -ItemType Directory
+New-Item -Path "./dev/pages/cmdlets/nightly" -ItemType Directory
+
+$nightlycmdlets = Get-ChildItem "./dev/documentation/*.md" | ForEach-Object{$_ | Select-Object -ExpandProperty Name}
+$releasedcmdlets = Get-ChildItem "./master/documentation/*.md" | ForEach-Object{$_ | Select-Object -ExpandProperty Name}
+
+foreach($nightlycmdlet in $nightlycmdlets)
+{
+    if(!$releasedcmdlets.Contains($nightlycmdlet))
+    {
+        Copy-Item "./dev/documentation/$nightlycmdlet" -Destination "./dev/pages/cmdlets/nightly"
+    }
+}
+Copy-Item -Path "./master/documentation/*.md" -Destination "./dev/pages/cmdlets/released" -Force
 
 # generate cmdlet toc
 
-$items = Get-ChildItem "./dev/pages/cmdlets/*.md"
+$items = Get-ChildItem "./dev/pages/cmdlets/released/*.md"
 $toc = ""
 foreach($item in $items)
 {
     $toc = $toc + "- name: $($item.Name -replace '.md','')`n  href: $($item.Name)`n"
 }
-$toc | Out-File "./dev/pages/cmdlets/toc.yml" -Force
+$toc | Out-File "./dev/pages/cmdlets/released/toc.yml" -Force
+
+$items = Get-ChildItem "./dev/pages/cmdlets/nightly/*.md"
+$toc = ""
+foreach($item in $items)
+{
+    $toc = $toc + "- name: $($item.Name -replace '.md','')`n  href: $($item.Name)`n"
+}
+$toc | Out-File "./dev/pages/cmdlets/nightly/toc.yml" -Force
 
 docfx build ./dev/pages/docfx.json
 
