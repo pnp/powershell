fab01f2ec3e4a51fac477cb6025e1ec09becda47
Erwin van Hunen <erwin.van.hunen@outlook.com>
2020-10-23 10:50:26 +0200

    Added support to cancel flow

--- src/Commands/Base/RegisterPnPManagementShellAccess.cs
+++ src/Commands/Base/RegisterPnPManagementShellAccess.cs
@@ -6,30 +6,44 @@ using PnP.PowerShell.Commands.Utilities;
 using System;
 using System.Diagnostics;
 using System.Management.Automation;
+using System.Threading;
 using System.Threading.Tasks;
+using TextCopy;
 
 namespace PnP.PowerShell.Commands.Base
 {
     [Cmdlet(VerbsLifecycle.Register, "ManagementShellAccess")]
     public class RegisterPnPManagementShellAccess : PSCmdlet
     {
+        CancellationTokenSource source;
+
         [Parameter(Mandatory = false)]
         public AzureEnvironment AzureEnvironment = AzureEnvironment.Production;
 
         protected override void ProcessRecord()
         {
+            source = new CancellationTokenSource();
+            CancellationToken token = source.Token;
+
             var endPoint = GenericToken.GetAzureADLoginEndPoint(AzureEnvironment);
 
             var application = PublicClientApplicationBuilder.Create(PnPConnection.PnPManagementShellClientId).WithAuthority($"{endPoint}/organizations/").WithRedirectUri("https://login.microsoftonline.com/common/oauth2/nativeclient").Build();
 
             application.AcquireTokenWithDeviceCode(new[] { "https://graph.microsoft.com/.default" }, codeResult =>
              {
-                 WriteUpdateMessage($"\n\nProvide consent for the PnP Management Shell application to access SharePoint.\n\nWe opened a browser and navigated to {codeResult.VerificationUrl}\n\nEnter code: {codeResult.UserCode}\n\n");
+                 ClipboardService.SetText(codeResult.UserCode);
+                 WriteUpdateMessage($"\n\nProvide consent for the PnP Management Shell application to access SharePoint.\n\nWe opened a browser and navigated to {codeResult.VerificationUrl}\n\nEnter code: {codeResult.UserCode} (we copied this code to your clipboard)\n\n");
                  BrowserHelper.LaunchBrowser(codeResult.VerificationUrl);
                  return Task.FromResult("");
 
-             }).ExecuteAsync().GetAwaiter().GetResult();
+             }).ExecuteAsync(token).GetAwaiter().GetResult();
         }
+
+        protected override void StopProcessing()
+        {
+            source.Cancel();
+        }
+
 
         private void WriteUpdateMessage(string message)
         {
