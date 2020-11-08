7224a5deff10941a8e314581cd86eacce88a9308
Erwin van Hunen <erwin.van.hunen@outlook.com>
2020-11-08 18:12:15 +0100

    Added docker build configuration

---
+++ docker/powershell/installModules.ps1
@@ -0,0 +1,17 @@
+$modules = @(
+    "PnP.PowerShell",
+    "Microsoft.PowerShell.SecretManagement",
+    "Microsoft.PowerShell.SecretStore"
+)
+
+Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+$ProgressPreference = "SilentlyContinue"
+
+foreach($module in $modules)
+{
+    Write-Host "Installing $module"
+    Install-Module -Name $module -AllowPrerelease | Out-Null
+}
+
+Register-SecretVault -Name "SecretStore" -ModuleName "Microsoft.PowerShell.SecretStore" -DefaultVault
+Set-SecretStoreConfiguration -Authentation None
