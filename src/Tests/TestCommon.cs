490c2b6c4c65683f1c5bb027dcef15f3ebccd462
Erwin van Hunen <erwin.van.hunen@outlook.com>
2020-11-12 21:26:44 +0100

    Updated config check

--- src/Tests/TestCommon.cs
+++ src/Tests/TestCommon.cs
@@ -17,14 +17,19 @@ namespace PnP.PowerShell.Tests
             var configuration = ConfigurationManager.OpenExeConfiguration("PnP.PowerShell.Tests.dll");
             if (configuration.AppSettings.Settings.Count == 0)
             {
-                throw new ConfigurationErrorsException("AppSettings is empty");
+                throw new ConfigurationErrorsException("AppSettings is empty. Did you copy App.config.sample to App.config and changed the values?");
             }
             // Read configuration data
             DevSiteUrl = configuration.AppSettings.Settings["SiteUrl"].Value;
-            var credentialManagerEntry = configuration.AppSettings.Settings["CredentialManagerLabel"].Value;
             if (string.IsNullOrEmpty(DevSiteUrl))
             {
                 throw new ConfigurationErrorsException("site url in App.config are not set up.");
+            }
+
+            var credentialManagerEntry = configuration.AppSettings.Settings["CredentialManagerLabel"].Value;
+            if (string.IsNullOrEmpty(credentialManagerEntry))
+            {
+                throw new ConfigurationErrorsException("CredentialmanagerLabel value is not set up in App.config");
             }
 
             // Trim trailing slashes
@@ -37,29 +42,9 @@ namespace PnP.PowerShell.Tests
         #region Properties
         public static string TenantUrl { get; set; }
         public static string DevSiteUrl { get; set; }
-        public static string Dev2SiteUrl { get; set; }
-        static string UserName { get; set; }
-        static SecureString Password { get; set; }
         static PSCredential Credentials { get; set; }
-        static string Realm { get; set; }
-        static string AppId { get; set; }
-        static string AppSecret { get; set; }
 
-        public static String AzureStorageKey
-        {
-            get
-            {
-                return ConfigurationManager.AppSettings["AzureStorageKey"];
-            }
-        }
 
-        public static string WebHookTestUrl
-        {
-            get
-            {
-                return ConfigurationManager.AppSettings["WebHookTestUrl"];
-            }
-        }
         #endregion
 
         #region Methods
@@ -76,25 +61,6 @@ namespace PnP.PowerShell.Tests
         public static ClientContext CreateTenantClientContext()
         {
             return CreateContext(TenantUrl, Credentials);
-        }
-
-        public static bool AppOnlyTesting()
-        {
-            if (!String.IsNullOrEmpty(ConfigurationManager.AppSettings["AppId"]) &&
-                !String.IsNullOrEmpty(ConfigurationManager.AppSettings["AppSecret"]) &&
-                String.IsNullOrEmpty(ConfigurationManager.AppSettings["SPOCredentialManagerLabel"]) &&
-                String.IsNullOrEmpty(ConfigurationManager.AppSettings["SPOUserName"]) &&
-                String.IsNullOrEmpty(ConfigurationManager.AppSettings["SPOPassword"]) &&
-                String.IsNullOrEmpty(ConfigurationManager.AppSettings["OnPremUserName"]) &&
-                String.IsNullOrEmpty(ConfigurationManager.AppSettings["OnPremDomain"]) &&
-                String.IsNullOrEmpty(ConfigurationManager.AppSettings["OnPremPassword"]))
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
         }
 
         private static ClientContext CreateContext(string contextUrl, PSCredential credentials)
