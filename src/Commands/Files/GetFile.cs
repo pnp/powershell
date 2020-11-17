b9d29e20a140d2403a34f432e268f6b9b531c243
Erwin van Hunen <erwin.van.hunen@outlook.com>
2020-11-17 07:32:39 +0100

    removed unused variable

--- src/Commands/Files/GetFile.cs
+++ src/Commands/Files/GetFile.cs
@@ -95,7 +95,7 @@ namespace PnP.PowerShell.Commands.Files
                         ClientContext.Load(file, f => f.Author, f => f.Length, f => f.ModifiedBy, f => f.Name, f => f.TimeCreated, f => f.TimeLastModified, f => f.Title);
                         ClientContext.ExecuteQueryRetry();
                     }                    
-                    catch (ServerException e)
+                    catch (ServerException)
                     {
                         // Assume the cause of the exception is that a principal cannot be found and try again without:
                         // Fallback in case the creator or person having last modified the file no longer exists in the environment such that the file can still be downloaded
