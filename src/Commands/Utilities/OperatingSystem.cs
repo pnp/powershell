178e9b3a72a5e66cf73f9b714ed57d6e42a85f1d
Erwin van Hunen <erwin.van.hunen@outlook.com>
2020-12-04 13:37:09 +0100

    Updated telemetry

--- src/Commands/Utilities/OperatingSystem.cs
+++ src/Commands/Utilities/OperatingSystem.cs
@@ -15,5 +15,25 @@ namespace PnP.PowerShell.Commands.Utilities
 
         public static bool IsLinux() =>
             RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+
+        public static string GetOSString()
+        {
+            if (IsWindows())
+            {
+                return "Windows";
+            }
+            else if (IsMacOS())
+            {
+                return "MacOS";
+            }
+            else if (IsLinux())
+            {
+                return "Linux";
+            }
+            else
+            {
+                return "Unknown";
+            }
+        }
     }
 }
