31007f839322a30a1e32a1e3d4588cfb754a8576
Erwin van Hunen <erwin.van.hunen@outlook.com>
2020-12-04 16:39:23 +0100

    added warning to -UseWebLogin explaining limitations

--- src/Commands/Base/ConnectOnline.cs
+++ src/Commands/Base/ConnectOnline.cs
@@ -686,6 +686,7 @@ namespace PnP.PowerShell.Commands.Base
             if (Utilities.OperatingSystem.IsWindows())
             {
                 return PnPConnectionHelper.InstantiateWebloginConnection(new Uri(Url.ToLower()), TenantAdminUrl, ForceAuthentication);
+                WriteWarning("Due to the way of authentication we are not able to retrieve on demand access tokens which is required for some cmdlets to work. None of the cmdlets that require the Microsoft Graph (Teams and Planner cmdlets) will work and some of the handlers in the PnP Provisioning Engine (Get-PnPSiteTemplate, Invoke-PnPSiteTemplate) will fail too. Consider moving to other types of authentication.");
             }
             else
             {
