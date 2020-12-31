66df52981cfc112ae318fe28fdc9ecc089137487
Erwin van Hunen <erwin.van.hunen@outlook.com>
2020-12-31 11:16:40 +0100

    reverted

--- build/Build-Release.ps1
+++ build/Build-Release.ps1
@@ -1,14 +1,10 @@
 $runPublish = $false
 
 $pnppowershell_hash = git ls-files -s ./src | git hash-object --stdin
-$local_pnppowershell_hash = Get-Content ./pnppowershell_hash.txt -Raw -ErrorAction SilentlyContinue
+$existing_pnppowershell_hash = Get-Content ./pnppowershell_hash.txt -Raw -ErrorAction SilentlyContinue
 
-$local_pnpframework_hash = Get-Content ./pnpframework_hash.txt -Raw -ErrorAction SilentlyContinue
+$existing_pnpframework_hash = Get-Content ./pnpframeworkhash.txt -Raw -ErrorAction SilentlyContinue
 $pnpframework_response = Invoke-RestMethod -Method Get -Uri "$($env:GITHUB_API_URL)/repos/pnp/pnpframework/branches/dev" -SkipHttpErrorCheck
-
-# $local_pnpcoresdk_hash = Get-Content ./pnpcoresdk_hash.txt -Raw -ErrorAction SilentlyContinue
-# $pnpcoresdk_response = Invoke-RestMethod -Method Get -Uri "$($env:GITHUB_API_URL)/repos/pnp/pnpcore/branches/dev" -SkipHttpErrorCheck
-
 if($null -ne $pnpframework_response)
 {
 	if($null -ne $pnpframework_response.commit)
@@ -16,23 +12,14 @@ if($null -ne $pnpframework_response)
 		$pnpframework_hash = $pnpframework_response.commit.sha
 	}
 }
+Write-host "Latest PnP PowerShell Commit hash $pnppowershell_hash" -ForegroundColor Yellow
+Write-Host "Stored PnP PowerShell Commit hash: $existing_pnppowershell_hash" -ForegroundColor Yellow
+Write-host "Latest PnP Framework Commit hash $pnpframework_hash" -ForegroundColor Yellow
+Write-Host "Stored PnP Framework Commit hash: $existing_pnpframework_hash" -ForegroundColor Yellow
 
-# if($null -ne $pnpcoresdk_response)
-# {
-# 	if($null -ne $pnpcoresdk_response.commit)
-# 	{
-# 		$pnpcoresdk_hash = $pnpcoresdk_response.commit.sha
-# 	}
-# }
-
-Write-Host "PnP PowerShell Commit: $pnppowershell_hash - $local_pnppowershell_hash" -ForegroundColor Yellow
-Write-Host "PnP Framework Commit: $pnpframework_hash - $local_pnpframework_hash" -ForegroundColor Yellow
-#Write-Host "PnP Core Commit: $pnpcoresdk_hash - $local_pnpcoresdk_hash" -ForegroundColor Yellow
-
-if ($local_pnppowershell_hash -ne $pnppowershell_hash || $local_pnpframework_hash -ne $pnpframework_hash) {
+if ($existing_pnppowershell_hash -ne $pnppowershell_hash || $existing_pnpframework_hash -ne $pnpframework_hash) {
 	Set-Content ./pnppowershell_hash.txt -Value $pnppowershell_hash -NoNewline -Force
 	Set-Content ./pnpframework_hash.txt -Value $pnpframework_hash -NoNewline -Force
-	#Set-Content ./pnpcoresdk_hash.txt -Value $pnpcoresdk_hash -NoNewline -Force
 	$runPublish = $true
 }
 
