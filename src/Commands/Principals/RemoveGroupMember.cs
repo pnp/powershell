5197b9f1a0a1540853e38f8b236decff4e0d25c2
Erwin van Hunen <erwin.van.hunen@outlook.com>
2021-01-02 23:52:02 +0100

    Updated groups handling

---
+++ src/Commands/Principals/RemoveGroupMember.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using System.Management.Automation;
+using Microsoft.SharePoint.Client;
+using PnP.Core;
+using PnP.PowerShell.Commands.Base.PipeBinds;
+
+namespace PnP.PowerShell.Commands.Principals
+{
+    [Cmdlet(VerbsCommon.Remove, "PnPGroupMember")]
+    [Alias("Remove-PnPUserFromGroup")]
+    public class RemoveUserFromGroup : PnPWebCmdlet
+    {
+
+        [Parameter(Mandatory = true)]
+        public string LoginName = string.Empty;
+
+        [Parameter(Mandatory = true)]
+        [Alias("Identity")]
+        public GroupPipeBind Group;
+
+        protected override void ExecuteCmdlet()
+        {
+            var group = Group.GetGroup(PnPContext);
+
+            if (group != null)
+            {
+                try
+                {
+                    var user = PnPContext.Web.EnsureUser(LoginName);
+                    group.RemoveUser(user.Id);
+                }
+                catch (PnP.Core.SharePointRestServiceException ex)
+                {
+                    throw new PSInvalidOperationException((ex.Error as SharePointRestError).Message);
+                }
+            }
+            else
+            {
+                throw new PSArgumentException("Group not found.");
+            }
+        }
+    }
+}
