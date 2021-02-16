589dfb72afbcf825a76fe773562b02a35136138e
Erwin van Hunen <erwin.van.hunen@outlook.com>
2021-02-16 11:18:29 +0100

    Updated to only return current version

--- src/Commands/Base/GetChangeLog.cs
+++ src/Commands/Base/GetChangeLog.cs
@@ -1,5 +1,6 @@
 using System.Management.Automation;
 using System.Net.Http;
+using System.Reflection;
 
 namespace PnP.PowerShell.Commands
 {
@@ -16,11 +17,40 @@ namespace PnP.PowerShell.Commands
             {
                 url = "https://raw.githubusercontent.com/pnp/powershell/dev/CHANGELOG.md";
             }
+
+            var assembly = Assembly.GetExecutingAssembly();
+#if !NETFRAMEWORK
+            var currentVersion = new SemanticVersion(assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion);
+#else
+            var currentVersion = new System.Version(((AssemblyFileVersionAttribute)assembly.GetCustomAttribute(typeof(AssemblyFileVersionAttribute))).Version);
+#endif
+
             var response = client.GetAsync(url).GetAwaiter().GetResult();
             if (response.IsSuccessStatusCode)
             {
+
                 var content = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
-                WriteObject(content);
+                if (Nightly)
+                {
+                    var versionString = $"{currentVersion.Major}.{currentVersion.Minor}.0";
+                    var match = System.Text.RegularExpressions.Regex.Match(content, $"(## \\[Current Nightly\\]\\n(.*)\\n)(## \\[{versionString}]\\n)", System.Text.RegularExpressions.RegexOptions.Singleline);
+
+                    if (match.Success)
+                    {
+                        WriteObject(match.Groups[1].Value);
+                    }
+                }
+                else
+                {
+                    var currentVersionString = $"{currentVersion.Major}.{currentVersion.Minor}.0";
+                    var previousVersionString = $"{currentVersion.Major}.{currentVersion.Minor-1}.0";
+                    var match = System.Text.RegularExpressions.Regex.Match(content, $"(## \\[{currentVersionString}\\]\\n(.*)\\n)(## \\[{previousVersionString}]\\n)", System.Text.RegularExpressions.RegexOptions.Singleline);
+
+                    if (match.Success)
+                    {
+                        WriteObject(match.Groups[1].Value);
+                    }
+                }
             }
             else
             {
