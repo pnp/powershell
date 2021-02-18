ed456993c0cce3519bb782d4ca1133d5b8b949c4
KoenZomers <koen@zomers.eu>
2021-02-19 00:03:51 +0100

    Added TemporarilyDisableAppBar cmdlet

---
+++ src/Commands/Admin/GetTemporarilyDisableAppBar.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.SharePoint.Client;
+
+using PnP.PowerShell.Commands.Base;
+using System.Management.Automation;
+
+namespace PnP.PowerShell.Commands.Admin
+{
+    [Cmdlet(VerbsCommon.Get, "PnPTemporarilyDisableAppBar")]
+    public class GetTemporarilyDisableAppBar : PnPAdminCmdlet
+    {
+        protected override void ExecuteCmdlet()
+        {
+            ClientContext.Load(Tenant, t => t.IsAppBarTemporarilyDisabled);
+            ClientContext.ExecuteQueryRetry();
+            WriteObject(Tenant.IsAppBarTemporarilyDisabled);
+        }
+    }
+}
---
+++ src/Commands/Admin/SetTemporarilyDisableAppBar.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.SharePoint.Client;
+
+using PnP.PowerShell.Commands.Base;
+using System.Management.Automation;
+
+namespace PnP.PowerShell.Commands.Admin
+{
+    [Cmdlet(VerbsCommon.Set, "PnPTemporarilyDisableAppBar")]
+    public class SetTemporarilyDisableAppBar : PnPAdminCmdlet
+    {
+        [Parameter(Mandatory = true, Position = 0, ValueFromPipeline = true)]
+        public bool Enabled;
+
+        protected override void ExecuteCmdlet()
+        {
+            Tenant.IsAppBarTemporarilyDisabled = Enabled;
+            ClientContext.ExecuteQueryRetry();
+        }
+    }
+}
