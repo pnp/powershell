c723cff0981ec4ea24ffbd2107f9892412671271
Koen Zomers <koen@zomers.eu>
2021-09-17 22:10:32 +0200

    Renamed service update to message center announcement, added documentation for the new cmdlets

--- src/Commands/ManagementApi/GetOffice365CurrentServiceStatus.cs
+++ src/Commands/ManagementApi/GetOffice365CurrentServiceStatus.cs
@@ -9,11 +9,7 @@ namespace PnP.PowerShell.Commands.ManagementApi
 {
     [Cmdlet(VerbsCommon.Get, "PnPOffice365CurrentServiceStatus")]
     [RequiredMinimalApiPermissions("https://manage.office.com/ServiceHealth.Read")]
-<<<<<<< HEAD
     [Obsolete("Use Get-PnPServiceCurrentHealth instead. It uses the Microsoft Graph backend which returns slightly different data. The Office Management API used by this cmdlet will be pulled by Microsoft in the future.")]    
-=======
-    [Obsolete("Use Get-PnPServiceCurrentHealth instead. It uses the Microsoft Graph backend which returns slightly different data. The API used by this cmdlet will be pulled by Microsoft in the future.")]    
->>>>>>> 185e1d36 (Added new Service Health cmdlets which use the Microsoft Graph backend)
     public class GetOffice365CurrentServiceStatus : PnPOfficeManagementApiCmdlet
     {
         [Parameter(Mandatory = false)]
