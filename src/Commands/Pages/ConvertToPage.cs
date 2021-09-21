6b3a56f4fe4ac4428b1db92901115cdab3d74ed0
KoenZomers <koen@zomers.eu>
2021-09-21 13:59:20 +0200

    Removing old alias as it mentions it would be removed in v1.5.0

--- src/Commands/Pages/ConvertToPage.cs
+++ src/Commands/Pages/ConvertToPage.cs
@@ -14,10 +14,7 @@ using PnP.PowerShell.Commands.Attributes;
 
 namespace PnP.PowerShell.Commands.Pages
 {
-
     [Cmdlet(VerbsData.ConvertTo, "PnPPage")]
-    [Alias("ConvertTo-PnPClientSidePage")]
-    [WriteAliasWarning("Please use 'ConvertTo-PnPPage'. The alias 'ConvertTo-PnPClientSidePage' will be removed in the 1.5.0 release")]
     public class ConvertToClientSidePage : PnPWebCmdlet
     {
         private static string rootFolder = "<root>";
