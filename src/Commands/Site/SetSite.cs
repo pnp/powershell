6eddd2b02dfdcf52e6f74a8a128d6c4431f95927
Koen Zomers <koen@zomers.eu>
2021-09-23 23:39:22 +0200

    Fixed issue 770

--- src/Commands/Site/SetSite.cs
+++ src/Commands/Site/SetSite.cs
@@ -3,8 +3,6 @@ using Microsoft.Online.SharePoint.TenantManagement;
 using Microsoft.SharePoint.Client;
 using PnP.Framework;
 using PnP.Framework.Entities;
-
-using PnP.PowerShell.Commands.Base;
 using PnP.PowerShell.Commands.Utilities;
 using System;
 using System.Collections.Generic;
@@ -15,7 +13,6 @@ namespace PnP.PowerShell.Commands.Site
     [Cmdlet(VerbsCommon.Set, "PnPSite")]
     public class SetSite : PnPSharePointCmdlet
     {
-
         private const string ParameterSet_LOCKSTATE = "Set Lock State";
         private const string ParameterSet_PROPERTIES = "Set Properties";
 
@@ -172,7 +169,6 @@ namespace PnP.PowerShell.Commands.Site
 
             if (IsTenantProperty())
             {
-
                 var tenantAdminUrl = UrlUtilities.GetTenantAdministrationUrl(context.Url);
                 context = context.Clone(tenantAdminUrl);
 
