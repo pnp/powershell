b3d4465ce37a26f6b4ab6ba4f39993e0522f6352
Koen Zomers <koen@zomers.eu>
2021-10-14 21:32:38 +0200

    Implemented logic for two new cmdlets (#1229)
    Co-authored-by: Gautam Sheth <gautamdsheth@outlook.com>

---
+++ src/Commands/Features/DisablePageScheduling.cs
@@ -0,0 +1,15 @@
+﻿using System.Management.Automation;
+using System;
+using Microsoft.SharePoint.Client;
+
+namespace PnP.PowerShell.Commands.Features
+{
+    [Cmdlet(VerbsLifecycle.Disable, "PnPPageScheduling")]
+    public class DisablePageScheduling : PnPWebCmdlet
+    {
+        protected override void ExecuteCmdlet()
+        {
+            CurrentWeb.DeactivateFeature(new Guid("E87CA965-5E07-4A23-B007-DDD4B5AFB9C7"));
+        }
+    }
+}
