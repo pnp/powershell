a44021069255bfd2cdd51600de0ad3a73f340137
KoenZomers <koen@zomers.eu>
2021-11-23 09:15:01 +0100

    Added optional flag IsDefault to allow retrieval of just the default or non default environments

--- src/Commands/PowerPlatform/Environment/GetPowerPlatformEnvironment.cs
+++ src/Commands/PowerPlatform/Environment/GetPowerPlatformEnvironment.cs
@@ -2,6 +2,7 @@
 using PnP.PowerShell.Commands.Base;
 using PnP.PowerShell.Commands.Utilities.REST;
 using System.Management.Automation;
+using System.Linq;
 
 namespace PnP.PowerShell.Commands.PowerPlatform.Environment
 {
@@ -11,9 +12,18 @@ namespace PnP.PowerShell.Commands.PowerPlatform.Environment
     [RequiredMinimalApiPermissions("https://management.azure.com/.default")]
     public class GetPowerPlatformEnvironment : PnPGraphCmdlet
     {
+        [Parameter(Mandatory = false)]
+        public bool? IsDefault;
+
         protected override void ExecuteCmdlet()
         {
-            var environments = GraphHelper.GetResultCollectionAsync<Model.PowerPlatform.Environment.Environment>(HttpClient, "https://management.azure.com/providers/Microsoft.ProcessSimple/environments?api-version=2016-11-01", AccessToken).GetAwaiter().GetResult();           
+            var environments = GraphHelper.GetResultCollectionAsync<Model.PowerPlatform.Environment.Environment>(HttpClient, "https://management.azure.com/providers/Microsoft.ProcessSimple/environments?api-version=2016-11-01", AccessToken).GetAwaiter().GetResult();
+
+            if(ParameterSpecified(nameof(IsDefault)) && IsDefault.HasValue)
+            {
+                environments = environments.Where(e => e.Properties.IsDefault.HasValue && e.Properties.IsDefault == IsDefault.Value);
+            }
+
             WriteObject(environments, true);
         }
     }
