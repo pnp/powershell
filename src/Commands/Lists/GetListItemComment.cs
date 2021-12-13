046dacbe22f1781067be0c0151344ec0d825df1c
Koen Zomers <koen@zomers.eu>
2021-12-13 22:27:48 +0100

    Removing en-us from all docs.microsoft.com links to satisfy with the requirements for documentation sync to docs.microsoft.com

---
+++ src/Commands/Lists/GetListItemComment.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Xml.Linq;
+using Microsoft.SharePoint.Client;
+
+using PnP.PowerShell.Commands.Base.PipeBinds;
+
+namespace PnP.PowerShell.Commands.Lists
+{
+    [Cmdlet(VerbsCommon.Get, "PnPListItemComment", DefaultParameterSetName = ParameterSet_ALLITEMS)]
+    public class GetListItemComment : PnPWebCmdlet
+    {
+        private const string ParameterSet_BYID = "By Id";
+        private const string ParameterSet_BYUNIQUEID = "By Unique Id";
+        private const string ParameterSet_BYQUERY = "By Query";
+        private const string ParameterSet_ALLITEMS = "All Items";
+        [Parameter(Mandatory = true, ValueFromPipeline = true, Position = 0, ParameterSetName = ParameterAttribute.AllParameterSets)]
+        public ListPipeBind List;
+
+        [Parameter(Mandatory = false, ParameterSetName = ParameterSet_BYID)]
+        public int Id = -1;
+
+        [Parameter(Mandatory = false, ParameterSetName = ParameterSet_BYUNIQUEID)]
+        public Guid UniqueId;
+
+        [Parameter(Mandatory = false, ParameterSetName = ParameterSet_BYQUERY)]
+        public string Query;
+
+        [Parameter(Mandatory = false, ParameterSetName = ParameterSet_BYQUERY)]
+        [Parameter(Mandatory = false, ParameterSetName = ParameterSet_ALLITEMS)]
+        public string FolderServerRelativeUrl;
+
+        [Parameter(Mandatory = false, ParameterSetName = ParameterSet_ALLITEMS)]
+        [Parameter(Mandatory = false, ParameterSetName = ParameterSet_BYID)]
+        [Parameter(Mandatory = false, ParameterSetName = ParameterSet_BYUNIQUEID)]
+        public string[] Fields;
+
+        [Parameter(Mandatory = false, ParameterSetName = ParameterSet_ALLITEMS)]
+		[Parameter(Mandatory = false, ParameterSetName = ParameterSet_BYQUERY)]
+        public int PageSize = -1;
+
+		[Parameter(Mandatory = false, ParameterSetName = ParameterSet_ALLITEMS)]
+		[Parameter(Mandatory = false, ParameterSetName = ParameterSet_BYQUERY)]
+		public ScriptBlock ScriptBlock;
+
+		protected override void ExecuteCmdlet()
+        {
+            var list = List.GetList(CurrentWeb);
+            if (list == null)
+                throw new PSArgumentException($"No list found with id, title or url '{List}'", "List");
+
+            if (HasId())
+            {
+                var listItem = list.GetItemById(Id);
+                if (Fields != null)
+                {
+                    foreach (var field in Fields)
+                    {
+                        ClientContext.Load(listItem, l => l[field]);
+                    }
+                }
+                else
+                {
+                    ClientContext.Load(listItem);
+                }
+                ClientContext.ExecuteQueryRetry();
+                WriteObject(listItem);
+            }
+            else if (HasUniqueId())
+            {
+                CamlQuery query = new CamlQuery();
+                var viewFieldsStringBuilder = new StringBuilder();
+                if (HasFields())
+                {
+                    viewFieldsStringBuilder.Append("<ViewFields>");
+                    foreach (var field in Fields)
+                    {
+                        viewFieldsStringBuilder.AppendFormat("<FieldRef Name='{0}'/>", field);
+                    }
+                    viewFieldsStringBuilder.Append("</ViewFields>");
+                }
+                query.ViewXml = $"<View Scope='RecursiveAll'><Query><Where><Or><Eq><FieldRef Name='GUID'/><Value Type='Guid'>{UniqueId}</Value></Eq><Eq><FieldRef Name='UniqueId' /><Value Type='Guid'>{UniqueId}</Value></Eq></Or></Where></Query>{viewFieldsStringBuilder}</View>";
+                
+                var listItem = list.GetItems(query);
+                ClientContext.Load(listItem);
+                ClientContext.ExecuteQueryRetry();
+                WriteObject(listItem);
+            }
+            else
+            {
+				CamlQuery query = HasCamlQuery() ? new CamlQuery { ViewXml = Query } : CamlQuery.CreateAllItemsQuery();
+                query.FolderServerRelativeUrl = FolderServerRelativeUrl;
+
+				if (Fields != null)
+                {
+                    var queryElement = XElement.Parse(query.ViewXml);
+
+                    var viewFields = queryElement.Descendants("ViewFields").FirstOrDefault();
+                    if (viewFields != null)
+                    {
+                        viewFields.RemoveAll();
+                    }
+                    else
+                    {
+                        viewFields = new XElement("ViewFields");
+                        queryElement.Add(viewFields);
+                    }
+
+                    foreach (var field in Fields)
+                    {
+                        XElement viewField = new XElement("FieldRef");
+                        viewField.SetAttributeValue("Name", field);
+                        viewFields.Add(viewField);
+                    }
+                    query.ViewXml = queryElement.ToString();
+                }
+
+                if (HasPageSize())
+                {
+                    var queryElement = XElement.Parse(query.ViewXml);
+
+                    var rowLimit = queryElement.Descendants("RowLimit").FirstOrDefault();
+                    if (rowLimit != null)
+                    {
+                        rowLimit.RemoveAll();
+                    }
+                    else
+                    {
+                        rowLimit = new XElement("RowLimit");
+                        queryElement.Add(rowLimit);
+                    }
+
+                    rowLimit.SetAttributeValue("Paged", "TRUE");
+                    rowLimit.SetValue(PageSize);
+
+                    query.ViewXml = queryElement.ToString();
+                }
+
+                do
+                {
+                    var listItems = list.GetItems(query);
+                    ClientContext.Load(listItems);
+                    ClientContext.ExecuteQueryRetry();
+
+                    WriteObject(listItems, true);
+
+                    if (ScriptBlock != null)
+                    {
+						ScriptBlock.Invoke(listItems);
+					}
+
+					query.ListItemCollectionPosition = listItems.ListItemCollectionPosition;
+                } while (query.ListItemCollectionPosition != null);
+            }
+        }
+
+        private bool HasId()
+        {
+            return Id != -1;
+        }
+
+        private bool HasUniqueId()
+        {
+            return UniqueId != null && UniqueId != Guid.Empty;
+        }
+
+        private bool HasCamlQuery()
+        {
+            return Query != null;
+        }
+
+        private bool HasFields()
+        {
+            return Fields != null;
+        }
+
+        private bool HasPageSize()
+        {
+            return PageSize > 0;
+        }
+    }
+}
