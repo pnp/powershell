6e1f914d8cffdca69bfd8712d4599d3c7f348b19
Ubuntu <adm_gbordier@proxy.tredunion.unplug.ms>
2022-01-28 15:42:42 +0000

    telemetry as singleton pattern

--- src/ALC/ApplicationInsights.cs
+++ src/ALC/ApplicationInsights.cs
@@ -6,21 +6,44 @@ using System.Linq;
 
 namespace PnP.PowerShell.ALC
 {
+    // implement telemtryclient as a singleton to avoid memory leaks
+    public static class TelemetryClientFactory
+  {
+    private static TelemetryClient _telemetryClient;
+ 
+    public static TelemetryClient GetTelemetryClient()
+    {
+            
+      if (_telemetryClient == null)
+      {
+        
+            TelemetryConfiguration config = TelemetryConfiguration.CreateDefault();
+            _telemetryClient = new TelemetryClient(config);
+            config.InstrumentationKey = "a301024a-9e21-4273-aca5-18d0ef5d80fb";
+            //config..Context.Session.Id = Guid.NewGuid().ToString();
+            _telemetryClient.Context.Cloud.RoleInstance = "PnPPowerShell";
+            _telemetryClient.Context.Device.OperatingSystem = Environment.OSVersion.ToString();
+
+            
+
+      }
+ 
+      return _telemetryClient;
+    }
+  }
+
     public class ApplicationInsights
     {
         private TelemetryClient telemetryClient;
-        private Dictionary<string, string> telemetryProperties;
+        private static  Dictionary<string, string> telemetryProperties;
+        
+
 
         public void Initialize(string serverLibraryVersion, string serverVersion, string initializationType, string assemblyVersion, string operatingSystem)
         {
-            TelemetryConfiguration config = TelemetryConfiguration.CreateDefault();
-            telemetryClient = new TelemetryClient(config);
-            config.InstrumentationKey = "a301024a-9e21-4273-aca5-18d0ef5d80fb";
-            //config..Context.Session.Id = Guid.NewGuid().ToString();
-            telemetryClient.Context.Cloud.RoleInstance = "PnPPowerShell";
-            telemetryClient.Context.Device.OperatingSystem = Environment.OSVersion.ToString();
-
-            telemetryProperties = new Dictionary<string, string>
+            telemetryClient = TelemetryClientFactory.GetTelemetryClient();
+            if (telemetryProperties == null ) {
+                telemetryProperties = new Dictionary<string, string>
             {
                 { "ServerLibraryVersion", serverLibraryVersion },
                 { "ServerVersion", serverVersion },
@@ -29,6 +52,7 @@ namespace PnP.PowerShell.ALC
                 { "Platform", "SPO" },
                 { "OperatingSystem", operatingSystem}
             };
+            }
         }
         public void TrackEvent(string cmdletName, Dictionary<string, string> properties = null)
         {
