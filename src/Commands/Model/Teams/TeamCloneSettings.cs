5d5337c6219ab547465efad4262e8331c96ae717
Reshmee011 <reshmee011@gmail.com>
2022-02-11 14:51:15 +0000

    Functionality added to clone a team

---
+++ src/Commands/Model/Teams/TeamCloneSettings.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.Graph;
+using PnP.PowerShell.Commands.Model.Graph;
+
+namespace PnP.PowerShell.Commands.Model.Teams
+{
+     public class TeamCloneInformation
+    {
+        public string DisplayName { get; set; }
+
+        public string Description { get; set; }
+
+        public string MailNickName { get; set; }
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public GroupVisibility Visibility { get; set; }
+
+        public string Classification { get; set; }
+        public ClonableTeamParts[] PartsToClone { get; set; }
+    }
+}
---
+++ src/Commands/Teams/CloneTeamsTeam.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Graph;
+using PnP.Framework.Provisioning.Model.Teams;
+using PnP.PowerShell.Commands.Attributes;
+using PnP.PowerShell.Commands.Base;
+using PnP.PowerShell.Commands.Base.PipeBinds;
+using PnP.PowerShell.Commands.Model.Graph;
+using PnP.PowerShell.Commands.Model.Teams;
+using PnP.PowerShell.Commands.Utilities;
+using PnP.PowerShell.Commands.Utilities.REST;
+using System;
+using System.Management.Automation;
+
+namespace PnP.PowerShell.Commands.Teams
+{
+    [Cmdlet(VerbsCommon.Copy, "PnPTeamsTeam")]
+    [RequiredMinimalApiPermissions("Team.Create")]
+    public class CloneTeamsTeam : PnPGraphCmdlet
+    {
+        [Parameter(Mandatory = true)]
+        public TeamsTeamPipeBind Identity;
+
+        [Parameter(Mandatory = true)]
+        public string DisplayName;
+
+        [Parameter(Mandatory = false)]
+        public TeamVisibility Visibility;
+
+        [Parameter(Mandatory = false)]
+        public string Classification;
+        /**
+        * There is a know issue that the mailNickname is currently ignored and cannot be set by the user
+        * However the mailNickname is still required by the payload so to deliver better user experience
+        * the CLI generates mailNickname for the user 
+        * so the user does not have to specify something that will be ignored.
+        * For more see: https://docs.microsoft.com/en-us/graph/api/team-clone?view=graph-rest-1.0#request-data
+        * This method has to be removed once the graph team fixes the issue and then the actual value
+        * of the mailNickname would have to be specified by the CLI user.
+        *  [Parameter(Mandatory = true)]
+        *   public string MailNickName;
+        */
+        [Parameter(Mandatory = false)]
+        public string Description;
+
+        [Parameter(Mandatory = false)]
+        public ClonableTeamParts[] PartsToClone;
+
+        protected override void ExecuteCmdlet()
+        {
+            var groupId = Identity.GetGroupId(HttpClient, AccessToken);
+            if (groupId != null)
+            {
+                TeamCloneInformation teamClone = new TeamCloneInformation();
+                teamClone.Classification = Classification;
+                teamClone.Description = Description;
+                teamClone.DisplayName = DisplayName;
+                teamClone.PartsToClone = PartsToClone;
+                /** copying displayName into MailNickName still required by the payload so to deliver better user experience
+                * but currently ignored and can't be set by user */
+                teamClone.MailNickName = DisplayName;
+                teamClone.Visibility = (GroupVisibility)Enum.Parse(typeof(GroupVisibility), Visibility.ToString());
+                var response = TeamsUtility.CloneTeamAsync(AccessToken, HttpClient, groupId, teamClone).GetAwaiter().GetResult();
+                if (!response.IsSuccessStatusCode)
+                {
+                    if (GraphHelper.TryGetGraphException(response, out GraphException ex))
+                    {
+                        if (ex.Error != null)
+                        {
+                            throw new PSInvalidOperationException(ex.Error.Message);
+                        }
+                    }
+                    else
+                    {
+                        WriteError(new ErrorRecord(new Exception($"Team clone failed"), "CLONEFAILED", ErrorCategory.InvalidResult, this));
+                    }
+                }
+            }
+            else
+            {
+                throw new PSArgumentException("Team not found");
+            }
+        }
+
+    }
+}
