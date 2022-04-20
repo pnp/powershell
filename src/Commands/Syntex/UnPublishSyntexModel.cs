806cd059b9f76a14dfa36c412a749c095c964aae
James May <james.may@draeger.com>
2022-04-20 21:29:21 +1000

    Syntex cmdlets: Add missing OutputType attributes for better command completion

--- src/Commands/Syntex/UnPublishSyntexModel.cs
+++ src/Commands/Syntex/UnPublishSyntexModel.cs
@@ -9,6 +9,7 @@ using System.Management.Automation;
 namespace PnP.PowerShell.Commands.Syntex
 {
     [Cmdlet(VerbsData.Unpublish, "PnPSyntexModel")]
+    [OutputType(typeof(SyntexPublicationResult))]
     public class UnPublishSyntexModel : PnPWebCmdlet
     {
         const string ParameterSet_SINGLE = "Single";
