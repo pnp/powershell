747eadd4a5b666b9d274c6fa1b24b101962bffb8
Gautam Sheth <gautamdsheth@outlook.com>
2022-04-21 18:10:20 +0300

    Fix build pipeline

--- src/Commands/Microsoft365Groups/NewMicrosoft365Group.cs
+++ src/Commands/Microsoft365Groups/NewMicrosoft365Group.cs
@@ -68,7 +68,7 @@ namespace PnP.PowerShell.Commands.Microsoft365Groups
             if (!Force)
             {
                 var candidate = Microsoft365GroupsUtility.GetGroupAsync(HttpClient, MailNickname, AccessToken, false, false).GetAwaiter().GetResult();
-                forceCreation = candidate == null || ShouldContinue($"The Microsoft 365 Group '{MailNickname} already exists. Do you want to create a new one?", Resources.Confirm);
+                forceCreation = candidate == null || ShouldContinue($"The Microsoft 365 Group '{MailNickname} already exists. Do you want to create a new one?", Properties.Resources.Confirm);
             }
             else
             {
