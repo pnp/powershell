07b9acbde0118e5c07fbf2a5a56672c8d5dff12c
Milan Holemans <11723921+milanholemans@users.noreply.github.com>
2022-05-03 08:45:17 +0200

    Implemented Get-PnpFlowRun cmdlet (#1819)

---
+++ src/Commands/Model/PowerPlatform/PowerAutomate/FlowRunTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PnP.PowerShell.Commands.Model.PowerPlatform.PowerAutomate
+{
+    public class FlowRunTrigger
+    {
+        public string Name { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
+
+        public DateTime ScheduledTime { get; set; }
+
+        public string OriginHistoryName { get; set; }
+
+        public string Code { get; set; }
+
+        public string Status { get; set; }
+    }
+}
