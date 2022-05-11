05b0cfc591698ee57b617df4762227e10f2c308f
Aleks <aleksandr.sapozhkov@soprasteria.com>
2022-05-11 13:57:33 +0200

    specifying platform for building

--- docker/Publish-UnpublishedImage.ps1
+++ docker/Publish-UnpublishedImage.ps1
@@ -47,7 +47,18 @@ $moduleVersions | % {
         $imageVersion = "$moduleVersion-$baseImageSuffix";
         Write-Host "Checking $imageVersion"
         if ( !( $publishedImageVersions -contains $imageVersion ) ) {
-            docker build --build-arg "PNP_MODULE_VERSION=$moduleVersion" --build-arg "BASE_IMAGE_SUFFIX=$baseImageSuffix" --build-arg "INSTALL_USER=$DOCKER_INSTALL_USER" --build-arg "SKIP_PUBLISHER_CHECK=$SKIP_PUBLISHER_CHECK" ./docker -f ./docker/pnppowershell.dockerFile --tag $DOCKER_USERNAME/$DOCKER_IMAGE_NAME`:$imageVersion;
+            $platform = switch ( $baseImageSuffix ) {
+                "alpine-3.14" {
+                    "linux/amd64"
+                }
+                "arm32v7-ubuntu-bionic" {
+                    "linux/arm/v7"
+                }
+                default { "Windows" }
+            }
+            $platform 
+            if ( $baseImageSuffix -eq "alpine-3.14" ) {}
+            docker build --platform=$platform --build-arg "PNP_MODULE_VERSION=$moduleVersion" --build-arg "BASE_IMAGE_SUFFIX=$baseImageSuffix" --build-arg "INSTALL_USER=$DOCKER_INSTALL_USER" --build-arg "SKIP_PUBLISHER_CHECK=$SKIP_PUBLISHER_CHECK" ./docker -f ./docker/pnppowershell.dockerFile --tag $DOCKER_USERNAME/$DOCKER_IMAGE_NAME`:$imageVersion;
             docker image tag $DOCKER_USERNAME/$DOCKER_IMAGE_NAME`:$imageVersion $DOCKER_USERNAME/$DOCKER_IMAGE_NAME`:latest;
             $plainStringPassword = [System.Net.NetworkCredential]::new("", $DOCKER_PASSWORD).Password;
             docker login -u $DOCKER_USERNAME -p "$plainStringPassword";
