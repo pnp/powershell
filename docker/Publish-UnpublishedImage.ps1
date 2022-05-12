91ccc10caccf23b4e93613e560aea11bf5909fca
Aleks <aleksandr.sapozhkov@soprasteria.com>
2022-05-12 12:44:28 +0200

    removing arm image

--- docker/Publish-UnpublishedImage.ps1
+++ docker/Publish-UnpublishedImage.ps1
@@ -47,12 +47,7 @@ $moduleVersions | % {
         $imageVersion = "$moduleVersion-$baseImageSuffix";
         Write-Host "Checking $imageVersion"
         if ( !( $publishedImageVersions -contains $imageVersion ) ) {
-            if ( $baseImageSuffix -eq "arm32v7-ubuntu-bionic" ) {
-                Write-Host "Using buildx --platform linux/arm/v7";
-                docker buildx build --load --platform linux/arm/v7 --build-arg "PNP_MODULE_VERSION=$moduleVersion" --build-arg "BASE_IMAGE_SUFFIX=$baseImageSuffix" --build-arg "INSTALL_USER=$DOCKER_INSTALL_USER" --build-arg "SKIP_PUBLISHER_CHECK=$SKIP_PUBLISHER_CHECK" ./docker -f ./docker/pnppowershell.dockerFile --tag $DOCKER_USERNAME/$DOCKER_IMAGE_NAME`:$imageVersion;
-            } else {
-                docker build --build-arg "PNP_MODULE_VERSION=$moduleVersion" --build-arg "BASE_IMAGE_SUFFIX=$baseImageSuffix" --build-arg "INSTALL_USER=$DOCKER_INSTALL_USER" --build-arg "SKIP_PUBLISHER_CHECK=$SKIP_PUBLISHER_CHECK" ./docker -f ./docker/pnppowershell.dockerFile --tag $DOCKER_USERNAME/$DOCKER_IMAGE_NAME`:$imageVersion;
-            }
+            docker build --build-arg "PNP_MODULE_VERSION=$moduleVersion" --build-arg "BASE_IMAGE_SUFFIX=$baseImageSuffix" --build-arg "INSTALL_USER=$DOCKER_INSTALL_USER" --build-arg "SKIP_PUBLISHER_CHECK=$SKIP_PUBLISHER_CHECK" ./docker -f ./docker/pnppowershell.dockerFile --tag $DOCKER_USERNAME/$DOCKER_IMAGE_NAME`:$imageVersion;
             docker image tag $DOCKER_USERNAME/$DOCKER_IMAGE_NAME`:$imageVersion $DOCKER_USERNAME/$DOCKER_IMAGE_NAME`:latest;
             $plainStringPassword = [System.Net.NetworkCredential]::new("", $DOCKER_PASSWORD).Password;
             docker login -u $DOCKER_USERNAME -p "$plainStringPassword";
