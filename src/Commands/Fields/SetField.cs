39281c5129a4b7b5c43ba81204f82ea95e3de862
James May <james.may@draeger.com>
2022-07-09 02:05:14 +1000

    fix

--- src/Commands/Fields/SetField.cs
+++ src/Commands/Fields/SetField.cs
@@ -4,12 +4,16 @@ using Microsoft.SharePoint.Client;
 
 using PnP.PowerShell.Commands.Base.PipeBinds;
 using System.Collections;
+using System.Linq;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using PnP.PowerShell.Commands.Utilities;
 
 namespace PnP.PowerShell.Commands.Fields
 {
-    [Cmdlet(VerbsCommon.Set, "PnPField")]
-    [OutputType(typeof(void))]
-    public class SetField : PnPWebCmdlet
+    [Cmdlet(VerbsCommon.Set, "PnPField", DefaultParameterSetName = "Field")]
+    [OutputType(typeof(Field))]
+    public class SetField : PnPWebCmdlet, IDynamicParameters
     {
         [Parameter(Mandatory = false, ValueFromPipeline = true)]
         public ListPipeBind List;
@@ -17,15 +21,20 @@ namespace PnP.PowerShell.Commands.Fields
         [Parameter(Mandatory = true, Position = 0, ValueFromPipeline = true)]
         public FieldPipeBind Identity = new FieldPipeBind();
 
-        [Parameter(Mandatory = true)]
+        [Parameter(Mandatory = false)]
         public Hashtable Values;
 
         [Parameter(Mandatory = false)]
         public SwitchParameter UpdateExistingLists;
 
+        object IDynamicParameters.GetDynamicParameters()
+            => DynamicParameters;
+
+        static readonly RuntimeDefinedParameterDictionary DynamicParameters
+            = PropertyDynamicParameters.GetDynamicParametersForSettablePropertiesOfType<Field>();
+
         protected override void ExecuteCmdlet()
         {
-            const string allowDeletionPropertyKey = "AllowDeletion";
             Field field = null;
             if (List != null)
             {
@@ -70,6 +79,18 @@ namespace PnP.PowerShell.Commands.Fields
                 }
             }
 
+            GetDynamicParameters().SetValuesFromParams(field, this);
+            SetValuesFromHashtable(field);
+
+            field.UpdateAndPushChanges(UpdateExistingLists);
+            ClientContext.Load(field);
+            ClientContext.ExecuteQueryRetry();
+            WriteObject(field);
+        }
+
+        private void SetValuesFromHashtable(Field field)
+        {
+            const string allowDeletionPropertyKey = "AllowDeletion";
             if (Values.ContainsKey(allowDeletionPropertyKey))
             {
                 ClientContext.Load(field, f => f.SchemaXmlWithResourceTokens);
@@ -79,7 +100,6 @@ namespace PnP.PowerShell.Commands.Fields
                 ClientContext.Load(field);
             }
             ClientContext.ExecuteQueryRetry();
-
             // Get a reference to the type-specific object to allow setting type-specific properties, i.e. LookupList and LookupField for Microsoft.SharePoint.Client.FieldLookup
             var typeSpecificField = field.TypedObject;
 
@@ -114,8 +134,6 @@ namespace PnP.PowerShell.Commands.Fields
                     }
                 }
             }
-            field.UpdateAndPushChanges(UpdateExistingLists);
-            ClientContext.ExecuteQueryRetry();
         }
     }
 }
---
+++ src/Commands/Utilities/DynamicParameters.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.SharePoint.Client;
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+
+namespace PnP.PowerShell.Commands.Utilities
+{
+    internal sealed class PropertyDynamicParameter : RuntimeDefinedParameter
+    {
+        public PropertyDynamicParameter(string name, Type parameterType, Collection<Attribute> attributes)
+            : base(name, parameterType, attributes)
+        { }
+
+        public override int GetHashCode()
+            => Name.GetHashCode();
+
+        public override bool Equals(object obj)
+            => obj is PropertyDynamicParameter pdp
+            && pdp.Name == Name;
+
+    }
+
+    internal static class PropertyDynamicParameters
+    {
+        public static RuntimeDefinedParameterDictionary GetDynamicParametersForSettablePropertiesOfType<TType>()
+            => GetDynamicParametersForSettablePropertiesOfType(typeof(TType));
+
+        public static RuntimeDefinedParameterDictionary GetDynamicParametersForSettablePropertiesOfType(Type type)
+        {
+            var assignableFromType =
+                type.Assembly.GetExportedTypes().Where(t => type.IsAssignableFrom(t));
+
+            var parameterDictionary = new RuntimeDefinedParameterDictionary();
+            var singleProps = new HashSet<string>();
+            var setablePublicProps = assignableFromType.SelectMany(t => t.GetProperties(
+                      System.Reflection.BindingFlags.Instance
+                    | System.Reflection.BindingFlags.Public
+                    | System.Reflection.BindingFlags.SetProperty
+                    ))
+                    .Where(p => !p.IsSpecialName
+                            && p.SetMethod is object
+                            && p.SetMethod.IsPublic
+                            && !p.SetMethod.ContainsGenericParameters
+                            && !p.GetIndexParameters().Any()
+                            && !string.Equals(p.Name, "Tag", StringComparison.Ordinal)
+                            && p.GetCustomAttributes(typeof(EditorBrowsableAttribute), true)
+                                  .OfType<EditorBrowsableAttribute>()
+                                  .All(a => a.State != EditorBrowsableState.Never))
+                    .GroupBy(p => p.Name, resultSelector: (name, pg) =>
+                    {
+                        var propTypes = pg.Select(p => p.PropertyType).Distinct();
+                        var pType = propTypes.Count() == 1
+                            ? propTypes.Single()
+                            : typeof(object);
+
+                        if (pType == typeof(bool))
+                            pType = typeof(SwitchParameter);
+
+                        var count = pg.Count();
+
+                        if (count == 1)
+                        {
+                            singleProps.Add(name);
+                        }
+                        return new
+                        {
+                            PropName = name,
+                            PropType = pType,
+                            SourceTypes = pg.Select(x => x.ReflectedType),
+                            SourceTypesKey = count == 1 || count == assignableFromType.Count() ? null : string.Join("_", pg.Select(x => x.ReflectedType.Name).OrderBy(x => x)),
+                        };
+                    });
+
+            foreach (var p in setablePublicProps)
+            {
+                var attributeCollection = new Collection<Attribute>();
+                if (p.SourceTypesKey != null)
+                    attributeCollection.Add(new ParameterAttribute() { Mandatory = false, ParameterSetName = p.SourceTypesKey });
+
+                foreach (var sourceType in p.SourceTypes)
+                    if (sourceType.Name != p.SourceTypesKey)
+                        attributeCollection.Add(new ParameterAttribute() { Mandatory = singleProps.Contains(sourceType.Name), ParameterSetName = sourceType.Name });
+
+                var runtimeParameter = new PropertyDynamicParameter(p.PropName, p.PropType, attributeCollection);
+                parameterDictionary.Add(runtimeParameter.Name, runtimeParameter);
+            }
+
+            var unAmbiguous = parameterDictionary.Values.OfType<PropertyDynamicParameter>()
+                .SelectMany(p => p.Attributes.OfType<ParameterAttribute>().Select(a => new { a.ParameterSetName, ParameterName = p.Name }))
+                .GroupBy(x => x.ParameterSetName, elementSelector: p => p.ParameterName) 
+                .GroupBy(e => string.Join(";", e.OrderBy(x => x)))
+                .Where(x => x.Count() == 1)
+                .Select(x=> x.Single().Key);
+
+            var unAmbiguousSet = new HashSet<string>(unAmbiguous);
+
+            foreach (var t in assignableFromType)
+            {
+                parameterDictionary.Add(t.Name, new RuntimeDefinedParameter(t.Name, typeof(SwitchParameter),
+                    new Collection<Attribute>() { new ParameterAttribute() { Mandatory = type.Name != t.Name && !unAmbiguousSet.Contains(t.Name), ParameterSetName = t.Name } }));
+            } 
+
+            return parameterDictionary;
+        }
+
+        public static void SetValuesFromParams(this RuntimeDefinedParameterDictionary dict, ClientObject obj, PSCmdlet cmdlet)
+            => SetValuesFromParams(dict, (object)obj.TypedObject, cmdlet);
+
+        public static void SetValuesFromParams(this RuntimeDefinedParameterDictionary dict, object obj, PSCmdlet cmdlet)
+        {
+            var objType = obj.GetType();
+
+            foreach (var dynParam in dict.Values.OfType<PropertyDynamicParameter>())
+            {
+                if (!cmdlet.MyInvocation.BoundParameters.TryGetValue(dynParam.Name, out var dynParamValueObj))
+                    continue;
+
+                if (dynParamValueObj is SwitchParameter switchParameter)
+                    dynParamValueObj = (bool)switchParameter;
+
+                objType.GetProperty(dynParam.Name, dynParam.ParameterType).SetValue(obj, dynParamValueObj);
+            }
+        }
+
+        public static IEnumerable<Type> BaseTypes(this Type t, Type stopBefore = null)
+        {
+            if (t == stopBefore) yield break;
+            while ((t = t.BaseType) != stopBefore)
+                yield return t;
+        }
+    }
+}
