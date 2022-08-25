678492aaa7205fedc5030c13e835e99d01d48e3c
Gautam Sheth <gautamdsheth@outlook.com>
2022-08-25 15:41:10 +0300

    Improved storage entity code

--- src/Commands/Admin/GetStorageEntity.cs
+++ src/Commands/Admin/GetStorageEntity.cs
@@ -1,9 +1,6 @@
 ﻿using System.Linq;
 using System.Management.Automation;
-using Microsoft.Online.SharePoint.TenantAdministration;
 using Microsoft.SharePoint.Client;
-
-using PnP.PowerShell.Commands.Base;
 using PnP.PowerShell.Commands.Enums;
 using System.Collections.Generic;
 using System.Text.Json;
@@ -22,33 +19,71 @@ namespace PnP.PowerShell.Commands
         protected override void ExecuteCmdlet()
         {
             string storageEntitiesIndex = string.Empty;
-            if (Scope == StorageEntityScope.Tenant)
-            {
-                var appCatalogUri = ClientContext.Web.GetAppCatalog();
-                if (appCatalogUri != null)
-                {
-                    using (var clonedContext = ClientContext.Clone(appCatalogUri))
-                    {
-                        storageEntitiesIndex = clonedContext.Web.GetPropertyBagValueString("storageentitiesindex", "");
-                    }
-                }
-                else
-                {
-                    WriteWarning("Tenant app catalog is not available on this tenant.");
-                }
-            }
-            else
+
+            if (Scope == StorageEntityScope.Site)
             {
                 var appcatalog = ClientContext.Site.RootWeb.SiteCollectionAppCatalog;
                 ClientContext.Load(appcatalog);
                 ClientContext.ExecuteQueryRetry();
                 if (appcatalog.ServerObjectIsNull == false)
                 {
-                    storageEntitiesIndex = ClientContext.Site.RootWeb.GetPropertyBagValueString("storageentitiesindex", "");
+                    if (ParameterSpecified(nameof(Key)))
+                    {
+                        var storageEntity = ClientContext.Site.RootWeb.GetStorageEntity(Key);
+                        ClientContext.Load(storageEntity);
+                        ClientContext.ExecuteQueryRetry();
+
+                        var storageEntityValue = new StorageEntity
+                        {
+                            Key = Key,
+                            Value = storageEntity.Value,
+                            Comment = storageEntity.Comment,
+                            Description = storageEntity.Description
+                        };
+                        WriteObject(storageEntityValue);
+                    }
+                    else
+                    {
+                        storageEntitiesIndex = ClientContext.Site.RootWeb.GetPropertyBagValueString("storageentitiesindex", "");
+                    }
                 }
                 else
                 {
                     WriteWarning("Site Collection App Catalog is not available on this site.");
+                }
+            }
+            else
+            {
+                var appCatalogUri = ClientContext.Web.GetAppCatalog();
+                if (appCatalogUri != null)
+                {
+                    using (var clonedContext = ClientContext.Clone(appCatalogUri))
+                    {
+                        if (ParameterSpecified(nameof(Key)))
+                        {
+                            var storageEntity = clonedContext.Site.RootWeb.GetStorageEntity(Key);
+                            clonedContext.Load(storageEntity);
+                            clonedContext.ExecuteQueryRetry();
+
+                            var storageEntityValue = new StorageEntity
+                            {
+                                Key = Key,
+                                Value = storageEntity.Value,
+                                Comment = storageEntity.Comment,
+                                Description = storageEntity.Description
+                            };
+
+                            WriteObject(storageEntityValue);
+                        }
+                        else
+                        {
+                            storageEntitiesIndex = clonedContext.Web.GetPropertyBagValueString("storageentitiesindex", "");
+                        }
+                    }
+                }
+                else
+                {
+                    WriteWarning("Tenant app catalog is not available on this tenant.");
                 }
             }
 
@@ -68,14 +103,7 @@ namespace PnP.PowerShell.Commands
                     };
                     storageEntities.Add(storageEntity);
                 }
-                if (ParameterSpecified(nameof(Key)))
-                {
-                    WriteObject(storageEntities.Where(k => k.Key == Key));
-                }
-                else
-                {
-                    WriteObject(storageEntities, true);
-                }
+                WriteObject(storageEntities, true);
             }
         }
     }
