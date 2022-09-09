259278d21944753e7efa972afcb689074b579487
James May <james.may@draeger.com>
2022-09-09 15:50:14 +1000

    RoleDefinitionPipeBind: specifying RoleDefinition by Id

--- src/Commands/Base/PipeBinds/RoleDefinitionPipeBind.cs
+++ src/Commands/Base/PipeBinds/RoleDefinitionPipeBind.cs
@@ -1,33 +1,53 @@
 ﻿using Microsoft.SharePoint.Client;
+
 using System;
 
 namespace PnP.PowerShell.Commands.Base.PipeBinds
 {
     public sealed class RoleDefinitionPipeBind
     {
-        private readonly RoleDefinition _roleDefinition;
-        private readonly string _name;
+        private readonly object _roleDefinition;
 
         public RoleDefinitionPipeBind(RoleDefinition definition)
         {
             _roleDefinition = definition;
         }
 
+        public RoleDefinitionPipeBind(int id)
+        {
+            _roleDefinition = id;
+        }
+
         public RoleDefinitionPipeBind(string name)
         {
-            _name = name;
+            _roleDefinition = name;
         }
-        
+
         public RoleDefinition GetRoleDefinition(Microsoft.SharePoint.Client.Site site)
+            => GetRoleDefinition(site.RootWeb);
+
+        public RoleDefinition GetRoleDefinition(Web web)
         {
-            if(_roleDefinition != null)
+            switch (_roleDefinition)
             {
-                return _roleDefinition;
+                case RoleDefinition rd:
+                    return rd;
+
+                case int id:
+                    var rdById = web.RoleDefinitions.GetById(id);
+                    web.Context.Load(rdById);
+                    web.Context.ExecuteQueryRetry();
+                    return rdById;
+
+                case string name:
+                    var rdByName = web.RoleDefinitions.GetByName(name);
+                    web.Context.Load(rdByName);
+                    web.Context.ExecuteQueryRetry();
+                    return rdByName;
+
+                default:
+                    throw new ArgumentNullException();
             }
-            var roleDefinition = site.RootWeb.RoleDefinitions.GetByName(_name);
-            site.Context.Load(roleDefinition);
-            site.Context.ExecuteQueryRetry();
-            return roleDefinition;
         }
     }
 }
