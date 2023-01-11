672e8efda98eeefd60306c331b6558330e469fc4
Koen Zomers <koen@zomers.eu>
2023-01-11 01:38:35 +0100

    Added types entities

---
+++ src/Commands/Model/AzureAD/AzureADServicePrincipalOauth2PermissionScopes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace PnP.PowerShell.Commands.Model.AzureAD
+{
+    /// <summary>
+    /// Oauth2PermissionScopes section within an Azure Active Directory Service Principal entity
+    /// </summary>
+    public class AzureADServicePrincipalOauth2PermissionScopes
+    {
+        [JsonPropertyName("adminConsentDescription")]
+        public string AdminConsentDescription { get; set; }
+
+        [JsonPropertyName("adminConsentDisplayName")]
+        public string AdminConsentDisplayName { get; set; }
+
+        [JsonPropertyName("id")]
+        public Guid? Id { get; set; }
+
+        [JsonPropertyName("isEnabled")]
+        public bool? IsEnabled { get; set; }
+
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+
+        [JsonPropertyName("userConsentDisplayName")]
+        public string UserConsentDisplayName { get; set; }
+
+        [JsonPropertyName("userConsentDescription")]
+        public string UserConsentDescription { get; set; }
+
+        [JsonPropertyName("value")]
+        public string Value { get; set; }                        
+    }
+}
