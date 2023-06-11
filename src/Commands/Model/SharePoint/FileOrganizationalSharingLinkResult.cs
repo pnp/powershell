d6c563dceca933c8b79b3295c5492c2063a0650e
Gautam Sheth <gautam.sheth@staffbase.com>
2023-06-11 20:53:47 +0300

    Feature: add organizational sharing link cmdlets for files and folder

---
+++ src/Commands/Model/SharePoint/FileOrganizationalSharingLinkResult.cs
@@ -0,0 +1,14 @@
+﻿using PnP.Core.Model.Security;
+using System.Collections.Generic;
+
+namespace PnP.PowerShell.Commands.Model.SharePoint
+{
+    public class FileOrganizationalSharingLinkResult
+    {
+        public string Id;
+
+        public ISharingLink Link;
+
+        public List<PermissionRole> Roles;
+    }
+}
---
+++ src/Commands/Security/AddFolderOrganizationalSharingLink.cs
@@ -0,0 +1,44 @@
+﻿using PnP.Core.Model.Security;
+using PnP.Core.Model.SharePoint;
+using PnP.PowerShell.Commands.Base.PipeBinds;
+using PnP.PowerShell.Commands.Model.SharePoint;
+using System.Management.Automation;
+
+namespace PnP.PowerShell.Commands.Security
+{
+    [Cmdlet(VerbsCommon.Add, "PnPFolderOrganizationalSharingLink")]
+    public class AddFolderOrganizationalSharingLink : PnPWebCmdlet
+    {
+        [Parameter(Mandatory = true)]
+        public FolderPipeBind Folder;
+
+        [Parameter(Mandatory = false)]
+        public ShareType ShareType = ShareType.View;
+
+        protected override void ExecuteCmdlet()
+        {
+            var serverRelativeUrl = string.Empty;
+            var ctx = Connection.PnPContext;
+
+            ctx.Web.EnsureProperties(w => w.ServerRelativeUrl);
+
+            IFolder folder = Folder.GetFolder(ctx);
+
+            var shareLinkRequestOptions = new OrganizationalLinkOptions()
+            {
+                Type = ShareType
+            };
+
+            var share = folder.CreateOrganizationalSharingLink(shareLinkRequestOptions);
+
+            FolderOrganizationalSharingLinkResult folderOrganizationalSharingLinkResult = new()
+            {
+                Id = share.Id,
+                Link = share.Link,
+                Roles = share.Roles,
+            };
+
+            WriteObject(folderOrganizationalSharingLinkResult);
+        }
+    }
+}
