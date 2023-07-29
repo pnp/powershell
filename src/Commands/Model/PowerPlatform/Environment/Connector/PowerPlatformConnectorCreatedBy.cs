46b8eb0866ac6ad3d77f6c390d0e237bdf5dad92
Siddharth Vaghasia <siddh.vaghasia@gmail.com>
2023-07-29 18:01:51 +0530

    added new command Get Powe Platform Connectors.

---
+++ src/Commands/Model/PowerPlatform/Environment/Connector/PowerPlatformConnectorCreatedBy.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PnP.PowerShell.Commands.Model.PowerPlatform.Environment
+{
+    public class PowerPlatformConnectorCreatedBy
+    {
+        [JsonPropertyName("id")]
+        public string id { get; set; }
+
+        [JsonPropertyName("displayName")]
+        public string displayName { get; set; }
+
+        [JsonPropertyName("email")]
+        public string email { get; set; }
+
+        [JsonPropertyName("type")]
+        public string type { get; set; }
+
+        [JsonPropertyName("tenantId")]
+        public string tenantId { get; set; }
+
+        [JsonPropertyName("userPrincipalName")]
+        public string userPrincipalName { get; set; }
+    }
+}
