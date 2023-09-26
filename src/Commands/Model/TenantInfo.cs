38aa7d2fd04dcfe49c52e7fb7dafea8c3a7dc646
Koen Zomers <koen@zomers.eu>
2023-09-26 15:45:02 +0200

    Documenting model and changing tenant Id to be a Guid

--- src/Commands/Model/TenantInfo.cs
+++ src/Commands/Model/TenantInfo.cs
@@ -1,11 +1,30 @@
-﻿
+﻿using System;
+
 namespace PnP.PowerShell.Commands.Model
 {
+    /// <summary>
+    /// Contains information regarding a tenant
+    /// </summary>
     public class TenantInfo
     {
-        public string TenantId { get; set; }
+        /// <summary>
+        /// Unique identifier of the tenant
+        /// </summary>
+        public Guid? TenantId { get; set; }
+
+        /// <summary>
+        /// The name of the string value shown to users when signing in to Entra ID
+        /// </summary>
         public string FederationBrandName { get; set; }
+
+        /// <summary>
+        /// The company name shown in places such as the admin portal
+        /// </summary>
         public string DisplayName { get; set; }
+
+        /// <summary>
+        /// The default domain name set on the tenant
+        /// </summary>
         public string DefaultDomainName { get; set; }
     }
 }
