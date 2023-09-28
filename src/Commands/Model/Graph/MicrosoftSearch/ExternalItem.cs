664c38310d4637cda13930e8bffaa2681d381456
Koen Zomers <koen@zomers.eu>
2023-09-28 10:03:39 +0200

    Adding `Set-PnPSearchExternalItem` to allow for indexing custom content into Microsoft Search (#3420)
    * Added functionality
    
    * Adding PR reference
    
    * Changed parameter name from Id to ItemId to clarify its the Id of the external item, added constraint of maximum 128 characters for the itemid
    
    * Fixing build error
    
    ---------
    
    Co-authored-by: Gautam Sheth <gautamdsheth@outlook.com>

---
+++ src/Commands/Model/Graph/MicrosoftSearch/ExternalItem.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace PnP.PowerShell.Commands.Model.Graph.MicrosoftSearch;
+
+/// <summary>
+/// Defines an external item used by Microsoft Search
+/// </summary>
+/// <seealso cref="https://learn.microsoft.com/graph/api/resources/externalconnectors-externalitem?#properties/>
+public class ExternalItem
+{
+    /// <summary>
+    /// Unique identifier of the external item within the connection
+    /// </summary>
+    [JsonPropertyName("id")]
+    public string Id { get; set; }
+
+    /// <summary>
+    /// The managed properties to provide to Microsoft Search about this external item
+    /// </summary>
+    [JsonConverter(typeof(JsonConverters.MicrosoftSearchExternalItemPropertyConverter))]
+    public Hashtable Properties { get; set; }
+
+    /// <summary>
+    /// The ACLs to assign to set permissions on this external item
+    /// </summary>
+    [JsonPropertyName("acl")]
+    public List<ExternalItemAcl> Acls { get; set; }
+
+    /// <summary>
+    /// The content of the external item
+    /// </summary>
+    [JsonPropertyName("content")]
+    public ExternalItemContent Content { get; set; }
+}
---
+++ src/Commands/Model/Graph/MicrosoftSearch/ExternalItemProperty.cs
@@ -0,0 +1,10 @@
+namespace PnP.PowerShell.Commands.Model.Graph.MicrosoftSearch;
+
+/// <summary>
+/// Defines managed metadata properties to use on the external item which will be in the Microsoft Search index
+/// </summary>
+/// <seealso cref="https://learn.microsoft.com/graph/api/externalconnectors-externalitem-create#request-body"/>
+public class ExternalItemProperty
+{
+
+}
