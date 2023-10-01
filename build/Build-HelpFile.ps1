8f240d9fb02d07024e5f7bfa3e556b87a32c2c5b
Koen Zomers <koen@zomers.eu>
2023-10-01 21:17:16 +0200

    Update Build-HelpFile.ps1

--- build/Build-HelpFile.ps1
+++ build/Build-HelpFile.ps1
@@ -6,37 +6,6 @@ if($IsLinux -or $isMacOS)
 	$destinationFolder = "$documentsFolder/PowerShell/Modules/PnP.PowerShell"
 }
 
-Try {
-	Write-Host "Generating documentation files for alias cmdlets" -ForegroundColor Yellow
-	# Load the Module in a new PowerShell session
-	$scriptBlock = {
-		Write-Host "Installing latest nightly of PnP PowerShell"
-  		Install-Module PnP.PowerShell -AllowPrerelease -Force
-
-  		Write-Host "Retrieving PnP PowerShell alias cmdlets"
-		$cmdlets = Get-Command -Module PnP.PowerShell | Where-Object CommandType -eq "Alias" | Select-Object -Property @{N="Alias";E={$_.Name}}, @{N="ReferencedCommand";E={$_.ReferencedCommand.Name}}
-		$cmdlets
-  		Write-Host "Retrieved alias cmdlets successfully"
-	}
-	$aliasCmdlets = Start-ThreadJob -ScriptBlock $scriptBlock | Receive-Job -Wait
-
-	Write-Host "  - $($aliasCmdlets.Length) found" -ForegroundColor Yellow
-
-	$aliasTemplatePageContent = Get-Content -Path "./pages/cmdlets/alias.md" -Raw
-
-	ForEach($aliasCmdlet in $aliasCmdlets)
-	{
-		$destinationFileName = "./documentation/$($aliasCmdlet.Alias).md"
-
-		Write-Host "    - Creating page for $($aliasCmdlet.Alias) being an alias for $($aliasCmdlet.ReferencedCommand) as $destinationFileName" -ForegroundColor Yellow
-		$aliasTemplatePageContent.Replace("%%cmdletname%%", $aliasCmdlet.Alias).Replace("%%referencedcmdletname%%", $aliasCmdlet.ReferencedCommand) | Out-File $destinationFileName -Force
-	}
-}
-Catch {
-	Write-Host "Error: Cannot generate alias documentation files"
-	Write-Host $_
-}
-
 $tempFolder = [System.IO.Path]::GetTempPath()
 
 $runsInAction = $("$env:RUNSINACTION")
