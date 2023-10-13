d9a68b4ed14416002c8cd78c5f81484c50619149
Gautam Sheth <gautam.sheth@staffbase.com>
2023-10-13 23:26:10 +0300

    Improve image

--- build/postCreateCommand.sh
+++ build/postCreateCommand.sh
@@ -1,6 +1,7 @@
-dotnet tool update -g docfx
-
-/usr/bin/pwsh -c 'Install-Module Microsoft.PowerShell.SecretStore,Microsoft.Powershell.SecretManagement,PlatyPS -Force'
-
-# Execute PS1 file in repo
+# Build the project
 /usr/bin/pwsh -c '/workspaces/powershell/build/Build-Debug.ps1'
+
+# Install optional modules
+/usr/bin/pwsh -c 'Install-Module Microsoft.PowerShell.SecretStore,Microsoft.Powershell.SecretManagement,PlatyPS -Force'
+# Add docfx package
+dotnet tool install --global docfx
