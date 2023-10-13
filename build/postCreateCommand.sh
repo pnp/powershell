ce7175a7cba8f6f59756f50b004b8b3ec3f4a2c8
Gautam Sheth <gautam.sheth@staffbase.com>
2023-10-13 21:54:38 +0300

    Improved container image

--- build/postCreateCommand.sh
+++ build/postCreateCommand.sh
@@ -1,3 +1,6 @@
 dotnet tool update -g docfx
 
- /usr/bin/pwsh -c 'Install-Module Microsoft.PowerShell.SecretStore,Microsoft.Powershell.SecretManagement,PlatyPS -Force'
+/usr/bin/pwsh -c 'Install-Module Microsoft.PowerShell.SecretStore,Microsoft.Powershell.SecretManagement,PlatyPS -Force'
+
+# Execute PS1 file in repo
+/usr/bin/pwsh -c '/workspaces/powershell/build/Build-Debug.ps1'
