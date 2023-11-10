f281a6ac392760f898248894217e298441dc2efb
jennywu <jennywu@microsoft.com_odspmdb>
2023-11-10 12:38:37 -0800

    [FileVersion] Add command to get progress for set version policy

---
+++ src/Commands/Site/GetSetVersionPolicyProgress.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.SharePoint.Client;
+
+using System;
+using System.Linq.Expressions;
+using System.Management.Automation;
+using System.Text.Json;
+using PnP.PowerShell.Commands.Model.SharePoint;
+
+namespace PnP.PowerShell.Commands.Site
+{
+    [Cmdlet(VerbsCommon.Get, "PnPSiteSetVersionPolicyProgress")]
+    [OutputType(typeof(PnP.PowerShell.Commands.Model.SharePoint.SetVersionPolicyProgress))]
+    public class GetSetVersionPolicyProgress : PnPSharePointCmdlet
+    {
+        protected override void ExecuteCmdlet()
+        {
+            ClientContext.Load(ClientContext.Site, s => s.Url);
+            var site = ClientContext.Site;
+            var ret = site.GetProgressForSetVersionPolicyForDocLibs();
+            ClientContext.ExecuteQueryRetry();
+
+            var progress = JsonSerializer.Deserialize<SetVersionPolicyProgress>(ret.Value);
+            progress.Url = site.Url;
+
+            if (string.Equals(progress.LastProcessTimeInUTC, DateTime.MinValue.ToString()))
+            {
+                progress.LastProcessTimeInUTC = string.Empty;
+            }
+
+            if (string.Equals(progress.CompleteTimeInUTC, DateTime.MinValue.ToString()))
+            {
+                progress.CompleteTimeInUTC = string.Empty;
+            }
+
+            WriteObject(progress);
+        }
+    }
+}
+
