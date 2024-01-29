ec43f40072077ef91e443236ddc489af1e46113b
Gautam Sheth <gautam.sheth@staffbase.com>
2024-01-29 13:08:17 +0200

    Feature #3700 - added cmdlet to change site archive state.

---
+++ src/Commands/Enums/SiteArchiveState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PnP.PowerShell.Commands.Enums
+{
+    public enum SPOArchiveState
+    {
+        Archived,
+        Active
+    }
+}
