cadc2d48bdbc779b758ed1d7360290e1b6309a95
blarrywang <blarrywang@microsoft.com_odspmdb>
2024-02-29 11:24:32 -0800

    Fix typo.

--- src/Commands/Lists/RemoveLibraryLevelFileVersionBatchDeleteJob.cs
+++ src/Commands/Lists/RemoveLibraryLevelFileVersionBatchDeleteJob.cs
@@ -9,7 +9,7 @@ using Resources = PnP.PowerShell.Commands.Properties.Resources;
 
 namespace PnP.PowerShell.Commands.Lists
 {
-    [Cmdlet(VerbsCommon.Remove, "LibrarytLevelFileVersionBatchDeleteJob")]
+    [Cmdlet(VerbsCommon.Remove, "LibraryLevelFileVersionBatchDeleteJob")]
     public class RemoveLibraryLevelFileVersionBatchDeleteJob : PnPWebCmdlet
     {
         [Parameter(Mandatory = true, ValueFromPipeline = true, Position = 0)]        
