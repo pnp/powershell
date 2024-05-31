76d1862706c2773316284d75b945c192812e61ac
Reshmee Auckloo <reshmee011@gmail.com>
2024-05-31 08:17:14 +0100

    cmdlet to add home site (#3989)
    Co-authored-by: Gautam Sheth <gautamdsheth@outlook.com>

---
+++ src/Commands/Admin/AddHomeSite.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Online.SharePoint.TenantAdministration;
+using Microsoft.SharePoint.Client;
+using PnP.PowerShell.Commands.Base;
+using System.Management.Automation;
+using PnP.PowerShell.Commands.Base.PipeBinds;
+using PnP.Core.Model;
+using System;
+
+namespace PnP.PowerShell.Commands.Admin
+{
+    [Cmdlet(VerbsCommon.Add, "PnPHomeSite")]
+    public class AddHomeSite : PnPAdminCmdlet
+    {
+        [Alias("Url")]
+        [Parameter(Mandatory = true)]
+        public string HomeSiteUrl;
+
+        protected override void ExecuteCmdlet()
+        {
+            Tenant.AddHomeSite(HomeSiteUrl, 999999, new System.Guid[0]);
+            AdminContext.ExecuteQueryRetry();
+        }
+    }
+}
