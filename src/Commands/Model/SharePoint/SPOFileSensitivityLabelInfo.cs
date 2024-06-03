71363e649acb166028000a782054968f7fdeeea5
Gautam Sheth <gautamdsheth@outlook.com>
2024-06-03 20:48:57 +0200

    Feature: added new cmdlet Get-PnPFileSensitivityLabelInfo (#3994)
    * Feature: added new cmdlet Get-PnPFileSensitivityLabelInfo
    
    * Update SPOFileSensitivityLabelInfo.cs
    
    ---------
    
    Co-authored-by: Gautam Sheth <gautam.sheth@staffbase.com>

---
+++ src/Commands/Model/SharePoint/SPOFileSensitivityLabelInfo.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Online.SharePoint.TenantAdministration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PnP.PowerShell.Commands.Model.SharePoint
+{
+    public class SPOFileSensitivityLabelInfo
+    {
+        public string LabelId { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public bool ProtectionEnabled { get; set; }
+
+        public string ParentLabelId { get; set; }
+
+        public SPOFileSensitivityLabelInfo(FileSensitivityLabelInfo label)
+        {
+            DisplayName = label.DisplayName;
+            LabelId = label.LabelId;            
+            ProtectionEnabled = label.ProtectionEnabled;
+            ParentLabelId = label.ParentLabelId;            
+        }
+    }
+}
