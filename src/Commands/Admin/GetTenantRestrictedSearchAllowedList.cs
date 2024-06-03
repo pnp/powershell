a0147f11c66dba950f2012f7f7d1d6efaa86353c
reshmee011 <reshmee011@gmail.com>
2024-06-03 23:14:06 +0100

    new cmdlet

---
+++ src/Commands/Admin/GetTenantRestrictedSearchAllowedList.cs
@@ -0,0 +1,17 @@
+﻿using System.Management.Automation;
+using Microsoft.SharePoint.Client;
+using PnP.PowerShell.Commands.Base;
+
+namespace PnP.PowerShell.Commands
+{
+    [Cmdlet(VerbsCommon.Get, " PnPTenantRestrictedSearchAllowedList")]
+    public class GetTenantRestrictedSearchAllowedList : PnPAdminCmdlet
+    {
+        protected override void ExecuteCmdlet()
+        {
+            var results = Tenant.GetSPORestrictedSearchAllowedList();
+            AdminContext.ExecuteQueryRetry();
+            WriteObject(results, true);
+        }
+    }
+}
