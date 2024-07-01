8daf121da69e110062bcd5eaff763323921c8196
Gautam Sheth <gautamdsheth@outlook.com>
2024-07-01 12:39:30 +0300

    Update Unlist-Nightly.ps1

--- build/Unlist-Nightly.ps1
+++ build/Unlist-Nightly.ps1
@@ -7,9 +7,10 @@ function CleanPackage {
     $xml = Invoke-WebRequest -Uri "https://www.powershellgallery.com/api/v2/Search()?`$orderby=Id&`$skip=0&`$top=50&searchTerm='$Package'&targetFramework=''&includePrerelease=true"
 
     $result = [xml] $xml
-
-    $entries = $result.feed.entry | Where-Object{$_.properties.Id -eq "PnP.PowerShell"} # There are other packages not owned by us that contain this string.
-
+    
+    # There are other packages not owned by us that contain this string.
+    $entries = $result.feed.entry | Where-Object{$_.properties.Id -eq "PnP.PowerShell"}
+    
     $sortedEntries = $entries | Sort-Object -Property @{Expression = {[System.Management.Automation.SemanticVersion]::Parse($_.properties.version)}; Descending=$false} | Where-Object {[System.Management.Automation.SemanticVersion]::Parse($_.properties.version).PreReleaseLabel -eq "nightly"} 
     $releasedEntries = $entries.Where({[System.Management.Automation.SemanticVersion]::Parse($_.properties.version).PreReleaseLabel -ne "nightly"} );
 
@@ -23,9 +24,10 @@ function CleanPackage {
     $key = $("$env:POWERSHELLGALLERY_API_KEY")   
     foreach($entry in $entries)
     {
+        Write-host "entries loop"
         if(!$entriesToKeep.Contains($entry))
         {
-            Write-Host "Removing version $($entry.properties.Version)"
+            Write-Host $($entry.properties.Version)
             nuget delete "package/$($entry.properties.Id)" $entry.properties.Version -ApiKey $key -Source https://www.powershellgallery.com/api/v2 -NonInteractive
         }
     }
