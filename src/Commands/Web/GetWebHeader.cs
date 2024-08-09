fcf1640ad5a5f7aa529727e0abe5156c03c98ba7
Gautam Sheth <gautam.sheth@staffbase.com>
2024-08-09 14:45:35 +0300

    Fix #4132 : issue with header background images in group connected sites.

--- src/Commands/Web/GetWebHeader.cs
+++ src/Commands/Web/GetWebHeader.cs
@@ -12,7 +12,7 @@ namespace PnP.PowerShell.Commands
         protected override void ExecuteCmdlet()
         {
             ClientContext.Load(CurrentWeb, p => p.SiteLogoUrl, p => p.LogoAlignment, p => p.HeaderLayout, p => p.HeaderEmphasis, p => p.HideTitleInHeader, p => p.AllProperties);
-            ClientContext.ExecuteQueryRetry();
+            ClientContext.ExecuteQueryRetry();            
 
             var response = new SharePointWebHeader
             {
@@ -21,7 +21,7 @@ namespace PnP.PowerShell.Commands
                 HeaderLayout = CurrentWeb.HeaderLayout,
                 HeaderEmphasis = CurrentWeb.HeaderEmphasis,
                 HideTitleInHeader = CurrentWeb.HideTitleInHeader,
-                HeaderBackgroundImageUrl = UrlUtilities.UrlDecode(CurrentWeb.AllProperties["BackgroundImageUrl"] as string)
+                HeaderBackgroundImageUrl = CurrentWeb.AllProperties.FieldValues.ContainsKey("BackgroundImageUrl") ? UrlUtilities.UrlDecode(CurrentWeb.AllProperties["BackgroundImageUrl"] as string) : string.Empty
             };
             WriteObject(response);
         }
