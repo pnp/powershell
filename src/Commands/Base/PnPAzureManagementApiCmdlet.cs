54822cd3b85bef959b8980e6adf24157f3ad631e
Gautam Sheth <gautam.sheth@staffbase.com>
2024-08-24 21:54:10 +0300

    Fix #3852: fix issue related to correct resource usage for power platform cmdlets

--- src/Commands/Base/PnPAzureManagementApiCmdlet.cs
+++ src/Commands/Base/PnPAzureManagementApiCmdlet.cs
@@ -12,7 +12,7 @@ namespace PnP.PowerShell.Commands.Base
         /// <summary>
         /// Returns an Access Token for the Microsoft Office Management API, if available, otherwise NULL
         /// </summary>
-        public string AccessToken => TokenHandler.GetAccessToken(this, "https://management.azure.com/.default", Connection);
+        public string AccessToken => TokenHandler.GetAccessToken(this, GetARMEndpoint(Connection), Connection);
 
         protected override void BeginProcessing()
         {
@@ -24,6 +24,33 @@ namespace PnP.PowerShell.Commands.Base
                     throw new PSInvalidOperationException("This cmdlet not work with a WebLogin/Cookie based connection towards SharePoint.");
                 }
             }
+        }
+
+        protected static string GetARMEndpoint(PnPConnection connection)
+        {
+            string endpoint;
+            switch (connection.AzureEnvironment)
+            {
+                case Framework.AzureEnvironment.Production:
+                    endpoint = "https://management.azure.com/.default";
+                    break;
+                case Framework.AzureEnvironment.China:
+                    endpoint = "https://management.chinacloudapi.cn/.default";
+                    break;
+                case Framework.AzureEnvironment.USGovernment:
+                    endpoint = "https://management.usgovcloudapi.net/.default";
+                    break;
+                case Framework.AzureEnvironment.USGovernmentHigh:
+                    endpoint = "https://management.usgovcloudapi.net/.default";
+                    break;
+                case Framework.AzureEnvironment.USGovernmentDoD:
+                    endpoint = "https://management.usgovcloudapi.net/.default";
+                    break;
+                default:
+                    endpoint = "https://management.azure.com/.default";
+                    break;
+            }
+            return endpoint;
         }
     }
 }
