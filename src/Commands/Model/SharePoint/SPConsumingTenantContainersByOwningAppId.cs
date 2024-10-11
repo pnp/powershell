80679ad24a9178672011c7aec2bb15641f41c44b
Koen Zomers <koen@zomers.eu>
2024-10-11 13:25:06 +0200

    Removing no longer used Graph usings

--- src/Commands/Model/SharePoint/SPConsumingTenantContainersByOwningAppId.cs
+++ src/Commands/Model/SharePoint/SPConsumingTenantContainersByOwningAppId.cs
@@ -1,12 +1,7 @@
-﻿using Microsoft.Graph;
-using Microsoft.Online.SharePoint.TenantAdministration;
+﻿using Microsoft.Online.SharePoint.TenantAdministration;
 using Microsoft.Online.SharePoint.TenantManagement;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PnP.PowerShell.Commands.Model.SharePoint
 {
