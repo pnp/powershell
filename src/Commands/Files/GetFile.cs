8967ab2a117b2d76553963aca087daa4b9c07f22
koskila <posti@koskila.net>
2024-10-25 20:40:47 +0300

    Fixes #4370 by changing absolute URLs to relative

--- src/Commands/Files/GetFile.cs
+++ src/Commands/Files/GetFile.cs
@@ -66,6 +66,9 @@ namespace PnP.PowerShell.Commands.Files
                 }
             }
 
+            // We can't deal with absolute URLs
+            Url = UrlUtility.MakeRelativeUrl(Url);
+
             // Remove URL decoding from the Url as that will not work. We will encode the + character specifically, because if that is part of the filename, it needs to stay and not be decoded.
             Url = Utilities.UrlUtilities.UrlDecode(Url.Replace("+", "%2B"));
 
