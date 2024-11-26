ca1f39a9574b5c2dd3044dc70ef318e1fcaf2cb1
Erwin van Hunen <erwin.van.hunen@outlook.com>
2024-11-26 16:47:30 +0100

    fixed path issue on MacOS

--- build/Build-HelpFile.ps1
+++ build/Build-HelpFile.ps1
@@ -1,7 +1,7 @@
 $documentsFolder = [environment]::getfolderpath("mydocuments")
 if($IsLinux -or $isMacOS)
 {
-	$destinationFolder = "$documentsFolder/.local/share/powershell/Modules/PnP.PowerShell"
+	$destinationFolder = "$HOME/.local/share/powershell/Modules/PnP.PowerShell"
 } else {
 	$destinationFolder = "$documentsFolder/PowerShell/Modules/PnP.PowerShell"
 }
