4ba9aada979b4fd45f9e1e4e0be04ae824bafe54
Janne Holm <janne.holm@meltlake.com>
2024-12-13 13:16:29 +0200

    Fix accesstoken scope for vanity domains

--- src/Commands/Provisioning/TokenRetrieval.cs
+++ src/Commands/Provisioning/TokenRetrieval.cs
@@ -30,7 +30,7 @@ namespace PnP.PowerShell.Commands.Provisioning
                         {
                             scope = $"https://{resource}/.default";
                         }
-                        return await authManager.GetAccessTokenAsync(new string[] { scope ?? $"https://{resource}" });
+                        return scope is null ? await authManager.GetAccessTokenAsync(new string[] { scope }) : await authManager.GetAccessTokenAsync($"https://{resource}");
                     }
                 }
             }
