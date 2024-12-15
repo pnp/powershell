c473439ce0609f5cc797e2dd2de14fbc3f48250a
Janne Holm <janne.holm@gmail.com>
2024-12-15 15:32:42 +0200

    Fix AccessToken scope for vanity domains (#4630)
    * Fix accesstoken scope for vanity domains
    
    * Change order of ternary
    
    ---------
    
    Co-authored-by: Janne Holm <janne.holm@meltlake.com>
    Co-authored-by: Gautam Sheth <gautamdsheth@outlook.com>

--- src/Commands/Provisioning/TokenRetrieval.cs
+++ src/Commands/Provisioning/TokenRetrieval.cs
@@ -30,7 +30,7 @@ namespace PnP.PowerShell.Commands.Provisioning
                         {
                             scope = $"https://{resource}/.default";
                         }
-                        return await authManager.GetAccessTokenAsync(new string[] { scope ?? $"https://{resource}" });
+                        return scope is null ? await authManager.GetAccessTokenAsync($"https://{resource}") : await authManager.GetAccessTokenAsync(new string[] { scope });
                     }
                 }
             }
