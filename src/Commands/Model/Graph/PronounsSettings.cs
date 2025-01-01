4dbd45c5eb3cc53d297a3df811e7c22749a68c63
Nishkalank <nishkalankbezawada@outlook.com>
2025-01-01 19:18:38 +0100

    Initial commit

---
+++ src/Commands/Model/Graph/PronounsSettings.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace PnP.PowerShell.Commands.Model.Graph
+{
+    /// <summary>
+    /// Contains a pronounsSettings property information
+    /// </summary>
+    /// <remarks>See https://learn.microsoft.com/en-us/graph/api/resources/pronounssettings?view=graph-rest-1.0</remarks>
+    public class PronounsSettings
+    {
+        /// <summary>
+        /// isEnabledInOrganization property name
+        /// </summary>
+        [JsonPropertyName("isEnabledInOrganization")]
+        public bool IsPronounsEnabledInOrganization { get; set; }
+
+    }
+}
---
+++ src/Commands/PronounSettings/GetPronounsSettings.cs
@@ -0,0 +1,26 @@
+﻿using PnP.PowerShell.Commands.Attributes;
+using PnP.PowerShell.Commands.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PnP.PowerShell.Commands.PronounSettings
+{
+    [Cmdlet(VerbsCommon.Get, "PnPTenantPronounsSettings")]
+    [RequiredApiApplicationPermissions("graph/PeopleSettings.Read.All")]
+    [RequiredApiDelegatedPermissions("graph/PeopleSettings.Read.All")]
+    public class GetPronounsSettings : PnPGraphCmdlet
+    {
+        protected override void ExecuteCmdlet()
+        {
+            WriteVerbose("Getting access token for Microsoft Graph");
+            var requestUrl = $"/v1.0/admin/people/pronouns";
+
+            var pronouns = RequestHelper.Get<Model.Graph.PronounsSettings>(requestUrl);
+            WriteObject(pronouns, false);
+        }
+    }
+}
