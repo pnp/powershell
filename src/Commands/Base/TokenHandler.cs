b97375b237683e7d03edd3c5826b975e6a6f483d
Gautam Sheth <gautamdsheth@outlook.com>
2025-06-23 13:51:11 +0300

    Fix #3249:  fix TokenHandler audience issue (#4975)
    * Fix #3249:  fix TokenHandler audience issue
    
    * Remove additional string escape
    
    * Update TokenHandler.cs

--- src/Commands/Base/TokenHandler.cs
+++ src/Commands/Base/TokenHandler.cs
@@ -383,9 +383,9 @@ namespace PnP.PowerShell.Commands.Base
                 var queryParams = new List<string>
     {
         "grant_type=client_credentials",
-        $"scope={Uri.EscapeDataString($"{resource}/.default")}",
+        $"scope={resource}",
         $"client_id={clientId}",
-        $"client_assertion_type={Uri.EscapeDataString("urn:ietf:params:oauth:client-assertion-type:jwt-bearer")}",
+        $"client_assertion_type=urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
         $"client_assertion={federatedToken}"
     };
 
