b1ca4662b8ad7fc0b6ced394c5ee676b0a387487
Gautam Sheth <gautamdsheth@outlook.com>
2025-06-28 11:56:31 +0300

    Improve interactive login messages for Azure AD app registration (#4991)
    * Improve interactive login messages for Azure AD app registration
    
    * Fix clipboard text setting for device login in Azure AD app registration
    
    ---------
    
    Co-authored-by: Gautam Sheth <gautam.sheth@staffbase.com>

--- src/Commands/AzureAD/RegisterAzureADApp.cs
+++ src/Commands/AzureAD/RegisterAzureADApp.cs
@@ -660,7 +660,7 @@ namespace PnP.PowerShell.Commands.AzureAD
                     using (var authManager = AuthenticationManager.CreateWithDeviceLogin(azureApp.AppId, Tenant, (deviceCodeResult) =>
                     {
                         ClipboardService.SetText(deviceCodeResult.UserCode);
-                        messageWriter.LogWarning($"\n\nCode {deviceCodeResult.UserCode} has been copied to your clipboard and a new tab in the browser has been opened. Please paste this code in there and proceed.\n\n");
+                        Host.UI.WriteWarningLine($"\n\nPlease login.\n\nWe opened a browser and navigated to {deviceCodeResult.VerificationUrl}\n\nEnter code: {deviceCodeResult.UserCode} (we copied this code to your clipboard)\n\nNOTICE: close the browser tab after you authenticated successfully to continue the process.");
                         BrowserHelper.OpenBrowserForInteractiveLogin(deviceCodeResult.VerificationUrl, BrowserHelper.FindFreeLocalhostRedirectUri(), cancellationTokenSource);
                         return Task.FromResult(0);
                     }, AzureEnvironment))
