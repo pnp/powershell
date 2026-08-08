f2225bf7b1f6efe018a968e2cf060ad9391ab286
Gautam Sheth <gautamdsheth@outlook.com>
2026-08-09 02:11:43 +0300

    Fix #4329 - issue with personal site permissions (#5438)
    * Fix #4329 - issue with personal site permissions
    
    * update docs

--- src/Commands/UserProfiles/NewPersonalSite.cs
+++ src/Commands/UserProfiles/NewPersonalSite.cs
@@ -1,12 +1,15 @@
 ﻿using System.Management.Automation;
 using Microsoft.SharePoint.Client;
 using Microsoft.SharePoint.Client.UserProfiles;
+using PnP.PowerShell.Commands.Attributes;
 using PnP.PowerShell.Commands.Base;
 
 namespace PnP.PowerShell.Commands.UserProfiles
 {
     [Cmdlet(VerbsCommon.New, "PnPPersonalSite")]
     [OutputType(typeof(void))]
+    [RequiredApiDelegatedPermissions("sharepoint/AllSites.FullControl", "sharepoint/User.ReadWrite.All")]
+    [RequiredApiApplicationPermissions("sharepoint/Sites.FullControl.All", "sharepoint/User.ReadWrite.All")]
     public class NewPersonalSite : PnPSharePointOnlineAdminCmdlet
     {
         [Parameter(Mandatory = true, Position = 0)]
